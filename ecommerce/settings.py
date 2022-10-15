47c1408a7ba40cd14daa97c1a431f58ae65bec95
mmshanida <mmshanida1@gmail.com>
2022-10-15 12:18:25 +0530

    new2

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@718pi(4$458)l-_skbrx4l@c8k6*8%z4_l6z)51)inc3%dv0&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['salesn.herokkuapp.com']
+ALLOWED_HOSTS = ['salesn.herokuapp.com']
 
 
 # Application definition
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
